39d478d07d13fdf32d721fb83bdf653b1e563e47
csquintas <csquintas2800@gmail.com>
2022-08-28 19:28:15 -0700

    forgot to add these 2

--- GameDay.py
+++ GameDay.py
@@ -14,7 +14,8 @@ from datetime import date as dt
 import re
 
 class GameDay:
-    stadium_info = [{"team":"Los Angeles Angels","address":"2000 Gene Autry Way, Anaheim, CA. 92806","lat":33.799572,"lng":-117.889031},
+    stadium_info = [
+                {"team":"Los Angeles Angels","address":"2000 Gene Autry Way, Anaheim, CA. 92806","lat":33.799572,"lng":-117.889031},
                 {"team":"Arizona Diamondbacks","address":"P.O. Box 2095, Phoenix, AZ. 85001","lat":33.452922,"lng":-112.038669},
                 {"team":"Atlanta Braves","address":"P.O. Box 4064, Atlanta, GA. 30302","lat":33.74691,"lng":-84.391239},
                 {"team":"Baltimore Orioles","address":"333 W. Camden Street, Baltimore, MD. 21201","lat":39.285243,"lng":-76.620103},
@@ -43,7 +44,8 @@ class GameDay:
                 {"team":"Tampa Bay Rays","address":"1 Tropicana Drive, St. Petersburg, FL 33705","lat":27.768487,"lng":-82.648191},
                 {"team":"Texas Rangers","address":"1000 Ballpark Way, Arlington, TX 76011","lat":32.750156,"lng":-97.081117},
                 {"team":"Toronto Blue Jays","address":"1 Blue Jay Way, Suite 3200, Toronto, ONT M5V 1J1","lat":43.641653,"lng":-79.3917},
-                {"team":"Washington Nationals","address":"1500 South Capitol Street SE, Washington, DC","lat":38.87,"lng":-77.01}]
+                {"team":"Washington Nationals","address":"1500 South Capitol Street SE, Washington, DC","lat":38.87,"lng":-77.01}
+            ]
         
     weather_lookup = {
             'clearday': 'Clear Skies',
@@ -81,15 +83,20 @@ class GameDay:
         self.games_times = []
         self.location_list = []
 
-    def GameData_Wrapper(self, date = None):
+    def GameDay_Wrapper(self, date = None):
         
         self.date = self.get_date(date)
         schedule = statsapi.schedule(self.date)
         self.doubleheaders = set()
         games_list = []
         games_times = []
+        home_pitcher_list = []
+        away_pitcher_list =[]
         location_list = []
         
+        print(f"Getting List of Games on {self.date}")
+        print("Matchups, Start Times, Probable Pitchers, and Weather will be added to a DataFrame")
+
         for game in schedule:
             
             home, away, matchup = self.get_teams(game)
@@ -102,12 +109,16 @@ class GameDay:
             
             games_list.append(matchup)
             games_times.append(game_time)
+            home_pitcher_list.append(home_pitcher)
+            away_pitcher_list.append(away_pitcher)
             location_list.append(location)
-        
+
         weather_list = self.get_weather(location_list)
         
         self.game_DF["Game"] = games_list
         self.game_DF["Time"] = games_times
+        self.game_DF["Home Pitcher"] = home_pitcher_list
+        self.game_DF["Away Pitcher"] = away_pitcher_list
         self.game_DF["Weather"] = weather_list
         print(f"DataFrame of Games on {self.date}: {self.game_DF}")
         return self.game_DF
@@ -116,7 +127,7 @@ class GameDay:
     
     def get_weather(self, locations):   
         
-        print("Fetching Weather Data: This may take 30 seconds")
+        print("Fetching Weather Data: This may take up to 30 seconds")
         url = "http://www.7timer.info/bin/api.pl?lon={lon}&lat={lat}&product={product}&output={type}"      
         weather_list = []
         
